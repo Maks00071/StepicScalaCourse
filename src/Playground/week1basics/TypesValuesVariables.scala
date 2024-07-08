dc429d70df832e1e3ba547bf61647e0ddcda5cad
maksim.korystov <maksim.korystov@glowbyteconsulting.com>
2024-07-08 16:40:04 +0300

    ADD TypesValuesVariables

---
+++ src/Playground/week1basics/TypesValuesVariables.scala
@@ -0,0 +1,42 @@
+package Playground.week1basics
+
+object TypesValuesVariables extends App {
+  val aString: String = "Hello"
+  println("String = " + aString)
+
+  val aChar: Char = 'a'
+  println("Char = " + aChar)
+
+  val aInt: Int = 5
+  println("Int = " + aInt)
+
+  val aLong: Long = 11L
+  println("Long = " + aLong)
+
+  val aFloat: Float = 2.0F
+  println("Float = " + aFloat)
+
+  val aDouble: Double = 2.0
+  println("Double = " + aDouble)
+
+  val aBoolean: Boolean = true
+  println("Boolean = " + aBoolean)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
