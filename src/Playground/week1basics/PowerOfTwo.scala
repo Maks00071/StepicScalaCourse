95ed178c89cfd8a774bdcde70077a8559c4759f7
Maks Korystov <corystov.maks@yandex.ru>
2024-07-11 23:55:47 +0300

    ADD Class PowerOfTwo

---
+++ src/Playground/week1basics/PowerOfTwo.scala
@@ -0,0 +1,16 @@
+package Playground.week1basics
+
+import scala.annotation.tailrec
+
+object PowerOfTwo extends App {
+  def powerTwo(pow: Int): BigInt = {
+    @tailrec
+    def loop(x: Int, accumulator: BigInt = 1): BigInt = {
+      if (x < 1) accumulator
+      else loop(x - 1, 2 * accumulator)
+    }
+    loop(pow)
+  }
+
+  println(powerTwo(0))
+}
